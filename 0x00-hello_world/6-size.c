fe2083579ebb37eb54206bdf022137a0d31e07dd
Sane93 <sanelendlovu787@gmail.com>
2023-02-16 12:28:39 -0800

    putfunction

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- Program prints the size of various types on the computer it is compiled and run on
+ * Return 0(success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float b;
+
+printf("Size of a char:%lu byte(s)\n",(unsigned long)sizeof(a)
