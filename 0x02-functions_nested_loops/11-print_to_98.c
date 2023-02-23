d1b44cfe625c0689b8623ab560f7304e89abc00f
Sane93 <sanelendlovu787@gmail.com>
2023-02-22 17:55:12 -0800

    11loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,9 +13,9 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d,", i);
+			printf("%d", i);
 			if (i != 98)
-				printf(", ");
+				printf(",");
 		}
 		printf("\n");
 	}
@@ -23,9 +23,9 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d,", i);
+			printf("%d", i);
 			if (i != 98)
-				printf(", ");
+				printf(",");
 		}
 		printf("\n");
 	}
@@ -33,12 +33,12 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d,", i);
+			printf("%d", i);
 			if (i != 98)
-				printf(", ");
+				printf(",");
 		}
 		printf("\n");
 	}
 	else if (n == 98)
-		printf("%d,", n);
+		printf("%d", n);
 }
