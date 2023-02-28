fdbae89850bd9c45274d6b76de4a7ab502fccf71
Sane93 <sanelendlovu787@gmail.com>
2023-02-28 12:21:10 -0800

    100atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,29 +8,22 @@
 
 int _atoi(char *s)
 {
-	int sign = 1, num = 0, i = 0;
+	int result = 0;
+	int sign = 1;
+	int i = 0;
 
-	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n'
-			|| s[i] == '\r' || s[i] == '\f' || s[i] == '\v')
+	while (s[i] != '\0')
 	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+			result = result * 10 + (s[i] - '0');
+
+		if (s[i] == '+' && s[i + 1] >= '0' && s[i + 1] <= '9')
+			break;
+
 		i++;
 	}
 
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (s[i] == '+')
-	{
-		i++;
-	}
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		num = num * 10 + (s[i] - '0');
-		i++;
-	}
-
-	return (sign * num);
+	return (sign * result);
 }
