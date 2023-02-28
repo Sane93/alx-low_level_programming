243a57ff3cce48a63a96ff8b7fb67a781874d46a
Sane93 <sanelendlovu787@gmail.com>
2023-02-28 12:37:43 -0800

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main- program that generates random valid passwords
+ * Return: Always 0
+ */
+#define PASSWORD_LENGTH 6
+
+int main(void)
+{
+	char password[PASSWORD_LENGTH + 1];
+	const char charset[] = "abcdefghijklmnopqrstuvwxyz
+		ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	srand(time(NULL));
+
+	for (int i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		password[i] = charset[rand() % sizeof(charset)];
+	}
+
+	password[PASSWORD_LENGTH] = '\0';
+	printf("%s\n", password);
+	return (0);
+}
