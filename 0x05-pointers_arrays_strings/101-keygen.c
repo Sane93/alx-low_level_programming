8bf46c2270b8dc38d4b9891839371b8e432854b2
Sane93 <sanelendlovu787@gmail.com>
2023-02-28 12:46:32 -0800

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,12 +10,13 @@
 
 int main(void)
 {
+	int i;
 	char password[PASSWORD_LENGTH + 1];
 	const char charset[] = "abcdefghijklmnopqrstQRSTUVWXYZ0123456789";
 
 	srand(time(NULL));
 
-	for (int i = 0; i < PASSWORD_LENGTH; i++)
+	for (i = 0; i < PASSWORD_LENGTH; i++)
 	{
 		password[i] = charset[rand() % sizeof(charset)];
 	}
